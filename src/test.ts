77b22aa1a05f8236586eeaeab608cc14e10f69c5
ittipat <39715559+ittipatken@users.noreply.github.com>
2026-06-25 19:09:12 +0700

    perf: update dependencies

--- src/test.ts
+++ src/test.ts
@@ -2,13 +2,10 @@
 
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserModule, platformBrowser } from '@angular/platform-browser';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  BrowserModule,
+  platformBrowser()
 );
